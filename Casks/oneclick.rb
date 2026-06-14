a39d11a65d6d9149a239451cf66ac7b59cb68914
NurikDz <nurikjim11@gmail.com>
2026-06-14 12:12:59 -0700

    Update oneclick cask version to 1.3

--- Casks/oneclick.rb
+++ Casks/oneclick.rb
@@ -1,6 +1,6 @@
-cask "oneclick" do
-  version "1.2"
-  sha256 "f7c836e3c57de5ed534d7fe37d551f74551dc226f8bcd55232d1b3105d0e8a19"
+\cask "oneclick" do
+  version "1.3"
+  sha256 "aeeaf18f5a145483da397afc6fed688fc6bd78cf87184243bb0a4fadbbd011ab"
 
   url "https://github.com/NurikDz/1clicklangs/releases/download/1clicklangs/OneClickLangs.dmg"
   name "1Click Language Store"
